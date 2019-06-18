96dfed6ca279bc6f68ebb02fa16e2c51229cea7f
andrew <amark1@its.jnj.com>
2019-06-18 16:15:35 -0500

    Included all external dependencies in the repo now. Pull and enjoy!

--- FKRA.bat
+++ FKRA.bat
@@ -26,30 +26,26 @@ set INPUTFOLDER=%~dp1
 
 REM # Settings and Variables
 REM # Random number used for temp folder and log
-set LAMEID=-%RANDOM%
+set RANDOMID=-%RANDOM%
 
 set STYLEROOT=%~dp0
 
-REM // LOGFILE - The path / filename where batch logs should be stored
-set LOGFILE=.\FKRA.LOG
-set TEMPFOLDER=%STYLEROOT%Temp
-set OUTPUTFOLDER=%STYLEROOT%Output
+set OUTPUTFOLDER=%STYLEROOT%\Output
 set SCRIPTSFOLDER=%STYLEROOT%
-set SAXONFOLDER=S:\Processors\saxon
-set XSLFOLDER=%STYLEROOT%XSL
+set SAXONFOLDER=%STYLEROOT%\saxon
+set XSLFOLDER=%STYLEROOT%\XSL
 
-REM // Environment variables for Saxon
-set CATALOG=S:\DITA_LFS_12\catalog-dita.xml
+REM // XML Catalog for Saxon
+set CATALOG=%STYLEROOT%\Catalog\catalog-dita.xml
+
 
 
 
 :start
-REM // Clear the logfile and put something sorta useful at the top
-ECHO [START] %0 > %LOGFILE% 2>&1
+ECHO [START] %0
 
-REM // Save a copy of the input files to temp folder and work on them from there
-MKDIR %TEMPFOLDER%
-COPY /Y %INPUTFOLDER%%INPUTFILE% %TEMPFOLDER%
+mkdir %OUTPUTFOLDER%
+
 
 
 
@@ -58,37 +54,33 @@ REM /*
 REM  * Generate output with Saxon and wkhtmltopdf
 REM  */
 ECHO ============================================================
-ECHO = Generating PDF
+ECHO = Generating Flesch-Kincaid PDF
 ECHO = Wait for "Press any key to continue..."
 ECHO ============================================================
 
 MKDIR %OUTPUTFOLDER% >> "%LOGFILE%" 2>&1
 
-REM // Create PDF
+REM // Create HTML and then PDF
+
 ECHO - Step 1/3: Create Summary Report HTML
-COPY /Y "%XSLFOLDER%\fkstyle.css" "%TEMPFOLDER%"
-
-CALL "%SAXONFOLDER%\Transform.exe" -catalog:"%CATALOG%" -s:"%TEMPFOLDER%\%INPUTFILE%" -xsl:"%XSLFOLDER%\FKRA.xsl" -o:"%TEMPFOLDER%\FKScore.html" detail=2
+COPY /Y "%XSLFOLDER%\fkstyle.css" "%OUTPUTFOLDER%"
+CALL ".\saxon\Transform.exe" -catalog:"%CATALOG%" -s:"%INPUTFOLDER%\%INPUTFILE%" -xsl:"%XSLFOLDER%\FKRA.xsl" -o:"%OUTPUTFOLDER%\FKScore.html" detail=2
 if %errorlevel% neq 0 goto exit_with_error
 
-
-ECHO - Step 2/3: Create Debug HTML (%TEMPFOLDER%\FKScore-debug.html)
-CALL "%SAXONFOLDER%\Transform.exe" -catalog:"%CATALOG%" -s:"%TEMPFOLDER%\%INPUTFILE%" -xsl:"%XSLFOLDER%\FKRA.xsl" -o:"%TEMPFOLDER%\FKScore-debug.html" detail=10
+ECHO - Step 2/3: Create Debug HTML (%OUTPUTFOLDER%\FKScore-debug.html)
+CALL ".\saxon\Transform.exe" -catalog:"%CATALOG%" -s:"%INPUTFOLDER%\%INPUTFILE%" -xsl:"%XSLFOLDER%\FKRA.xsl" -o:"%OUTPUTFOLDER%\FKScore-debug.html" detail=10
 if %errorlevel% neq 0 goto exit_with_error
-
 
 ECHO - Step 3/3: Convert HTML to PDF
-ECHO --SKIPPED--
-goto :EOF
+CALL ".\wkhtmltox\bin\wkhtmltopdf.exe" --print-media-type --page-size Letter -O portrait --dpi 115 --header-line --header-left [date] --header-center "Flesch Kincaid Readability Assessment" --header-right "Page [page]/[toPage]" "%OUTPUTFOLDER%\FKScore.html" "%OUTPUTFOLDER%\FKScore%RANDOMID%.pdf"
 
-CALL "%TOOLSFOLDER%\wkhtmltopdf32\bin\wkhtmltopdf.exe" --print-media-type --page-size Letter -O portrait --dpi 115 --header-line --header-left [date] --header-center "Flesch Kincaid Readability Assessment" --header-right "Page [page]/[toPage]" "%TEMPFOLDER%\FKScore.html" "%OUTPUTFOLDER%\%VASONTID%-fkra.pdf" >> "%LOGFILE%" 2>&1
 
 
 
 REM // Exit points
 :exit_no_error
 REM // Put something sorta useful at the bottom
-ECHO [END] %0 >> "%LOGFILE%" 2>&1
+ECHO [END] %0 
 echo FKRA Done
 echo .
 pause
@@ -98,9 +90,8 @@ exit /B 0
 
 :exit_with_error
 REM // Put something sorta useful at the bottom
-ECHO [END (Errors)] %0 >> "%LOGFILE%" 2>&1
-echo FKRA Done (with errors: See %LOGFILE%)
+ECHO [END (Errors)] %0
+echo FKRA Done (with errors)
 echo .
 pause
-start notepad "%LOGFILE%"
 exit /B 1
